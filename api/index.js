20b34f60054c08d5a5149cefa78ae6b55cbc62ca
meteoben <meteoben@gmail.com>
2024-01-15 08:00:37 +0100

    mise en commentaire websocket

--- api/index.js
+++ api/index.js
@@ -4,6 +4,23 @@ const cors = require('cors');
 app.use(cors())
 console.log(app)
 
+
+
+// const io = require('socket.io')(server);
+
+// io.on('connection', client => {
+//   client.on('reply', () => { console.log("a reply detected!")}); // listen to the event
+// });
+
+// server.listen(PORT, ()=>{
+//   console.log("Application running successfully on port: "+PORT);
+// });
+
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Hello from our server!')
 })
